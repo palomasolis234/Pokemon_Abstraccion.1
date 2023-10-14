3701a76970f4cc7cd781154d7149ded650a771f8
palomasolis234 <132014943+palomasolis234@users.noreply.github.com>
2023-10-13 23:15:55 -0600

    Create Tipo.java

---
+++ Tipo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pokemon_Abstraccion;
+
+/**
+ *
+ * @author palom
+ */
+public enum Tipo {
+    PLANTA, VENENO, BICHO, 
+    DRAGON, ELECTRICO, FANTASMA, 
+    FUEGO, HADA, HIELO,
+    LUCHA, NORMAL, ACERO,
+    PSIQUICO, ROCA, SINESTRO,
+    TIERRA, AGUA, VOLADOR
+}
